b383709b0c0a97273f95efa41f16b8e1739e0791
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-08-28 12:29:27 -0300

    feat: create totoro screen

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Home from "../screens/Home";
+import Totoro from "../screens/Totoro";
 
 const Stack = createNativeStackNavigator();
 
@@ -7,6 +9,7 @@ const StackRoutes = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Totoro" component={Totoro}/>
         </Stack.Navigator>
     );
 }
---
+++ src/screens/Totoro/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Totoro() {
+    return (
+        <View>
+            <Text>Totoro</Text>
+        </View>
+    );
+}
